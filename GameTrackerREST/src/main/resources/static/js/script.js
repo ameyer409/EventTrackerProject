6b96c9e4a34dfbb3108350dfe540565b5c1235a2
Allan Meyer <ameyer409@yahoo.com>
2024-02-26 10:29:30 -0500

    update function added, small bug still exists

--- GameTrackerREST/src/main/resources/static/js/script.js
+++ GameTrackerREST/src/main/resources/static/js/script.js
@@ -12,12 +12,10 @@ function init() {
 	details.style.display = "none";
 
 	loadAllGames();
-	
+
 	let button = document.getElementById('detailsbtn');
-	console.log(button);
 	button.addEventListener('click', function(e) {
 		e.preventDefault();
-		console.log('click');
 		showTable();
 	});
 
@@ -55,6 +53,7 @@ function init() {
 	})
 
 	document.addGameForm.addGame.addEventListener('click', function(e) {
+		console.log("clicks received");
 		e.preventDefault();
 		let hof = document.addGameForm.hallOfFame.value;
 		if (hof === 'on') {
@@ -77,8 +76,33 @@ function init() {
 		if (game.name !== '' && game.genre !== '') {
 			createGame(game);
 		}
-
-
+	});
+	
+	document.updateForm.updateSubmit.addEventListener('click', function(e) {
+		e.preventDefault();
+		console.log("receiving clicks");
+		let hof = document.updateForm.hallOfFame.value;
+		if (hof === 'on') {
+			hof = true;
+		}
+		else {
+			hof = false;
+		}
+		game = {
+			id: document.updateForm.id.value,
+			name: document.updateForm.name.value,
+			genre: document.updateForm.genre.value,
+			rating: document.updateForm.rating.value,
+			score: document.updateForm.score.value,
+			hallOfFame: hof,
+			releaseDate: new Date(document.updateForm.release.value),
+			completedDate: new Date(document.updateForm.completion.value),
+			description: document.updateForm.description.value,
+			review: document.updateForm.review.value
+		};
+		if (game.name !== '' && game.genre !== '') {
+			updateGame(game);
+		}
 	});
 
 }
@@ -108,8 +132,10 @@ function displayGames(games) {
 	let game = games[0];
 	let tr = document.createElement('tr');
 	for (let field in game) {
-		if (field !== 'id' && field !== 'hallOfFame' && field !== 'createdAt' && field !== 'updatedAt') {
+		if (field !== 'id' && field !== 'hallOfFame' && field !== 'createdAt' && field !== 'updatedAt'
+			&& field !== 'description' && field !== 'review') {
 			let th = document.createElement('th');
+			th.scope = 'col';
 			th.textContent = field;
 			tr.appendChild(th);
 		}
@@ -118,27 +144,30 @@ function displayGames(games) {
 
 	let tbody = document.getElementById('gameEntries');
 	tbody.textContent = '';
+	tbody.className = 'table-group-divider';
 
 	for (let game of games) {
 		let tr = document.createElement('tr');
 		tr.gameId = game.id
+		tr.className = "";
 		for (let field in game) {
-			if (field !== 'id' && field !== 'hallOfFame' && field !== 'createdAt' && field !== 'updatedAt') {
+			if (field !== 'id' && field !== 'hallOfFame' && field !== 'createdAt' && field !== 'updatedAt'
+				&& field !== 'description' && field !== 'review') {
 				let td = document.createElement('td');
 				td.textContent = '';
 				if (field !== null) {
 					td.textContent = game[field];
+					td.className = "";
 				}
 				tr.appendChild(td);
 			}
 		}
 		tr.addEventListener('click', function(e) {
-			console.log(tr.gameId);
 			getGame(tr.gameId);
 		});
 		tbody.appendChild(tr);
 	}
-
+	showTable();
 }
 
 function getGame(gameId) {
@@ -172,21 +201,123 @@ function getGame(gameId) {
 	xhr.send();
 }
 
+function updateDetails(game) {
+	let table = document.getElementById('gameList');
+	let details = document.getElementById('gameDetails');
+	table.style.display = "none";
+	details.style.display = "block";
+	details = document.getElementById('gameDetailsBody');
+	details.textContent = '';
+	let button = document.getElementById('updatebtn');
+	button.style.display = "none";
+	let form = document.getElementById('updateForm');
+	form.style.display = "block";
+console.log(game.id);
+console.log(game.name);
+	
+	let input = document.createElement('input');
+	input.type = 'hidden';
+	input.id = 'id';
+	input.value = game.id;
+	form.appendChild(input);
+	
+	//Name Field
+	input = document.getElementById('name');
+	input.value = game.name
+	//Genre Field
+	input = document.getElementById('genre');
+	input.value = game.genre
+	//Rating Field
+	//let select = document.getElementsByName('rating');
+	//console.log(select);	
+	//select.value = game.rating;
+	
+	//Score Field
+	input = document.getElementById('score');
+	input.value = game.score;
+	//Hall Of Fame Field
+	input = document.getElementById('hallOfFame');
+	input.checked = game.hallOfFame;
+	//Release Date Field
+	input = document.getElementById('release');
+	if (game.releaseDate !== null) {
+		let date = game.releaseDate.split("T");
+		input.defaultValue = date[0];
+	}
+	//completed Date Field
+	input = document.getElementById('completion');
+	if (game.completedDate !== null) {
+		date = game.completedDate.split("T");
+		input.defaultValue = date[0];
+	}
+	//Description Field
+	input = document.getElementById('description');
+	input.value = game.description
+	//Review Field
+	input = document.getElementById('review');
+	input.value = game.review
+
+	details.appendChild(form);
+	
+	
+}
+
 function showDetails(game) {
 	let table = document.getElementById('gameList');
 	let details = document.getElementById('gameDetails');
 	table.style.display = "none";
 	details.style.display = "block";
-	
+	details = document.getElementById('gameDetailsBody');
+	details.textContent = '';
+	let button = document.getElementById('updatebtn');
+	button.style.display = "inline";
+	let form = document.getElementById('updateForm');
+	form.style.display = "none";
+
+
+	let header = document.createElement('h4');
+	header.textContent = game.name;
+	details.appendChild(header);
+	header = document.createElement('h5');
+	header.textContent = game.genre;
+	details.appendChild(header);
+	header = document.createElement('div');
+	header.textContent = "ESRB rating: " + game.rating;
+	details.appendChild(header);
+	header = document.createElement('div');
+	header.textContent = "Score: " + game.score;
+	details.appendChild(header);
+	header = document.createElement('div');
+	header.textContent = "description: ";
+	header.style.fontWeight = "bold";
+	let textBlock = document.createElement('p');
+	textBlock.textContent = game.description;
+	textBlock.style.fontWeight = "normal";
+	header.appendChild(textBlock);
+	details.appendChild(header);
+	header = document.createElement('div');
+	header.textContent = "review: ";
+	header.style.fontWeight = "bold";
+	textBlock = document.createElement('p');
+	textBlock.textContent = game.review;
+	textBlock.style.fontWeight = "normal";
+	header.appendChild(textBlock);
+	details.appendChild(header);
+
+	let updateButton = document.getElementById('updatebtn');
+	updateButton.addEventListener('click', function(e) {
+		e.preventDefault();
+		updateDetails(game);
+	});
 
 }
 
-function showTable(){
+function showTable() {
 	let table = document.getElementById('gameList');
 	let details = document.getElementById('gameDetails');
 	table.style.display = "block";
 	details.style.display = "none";
-	
+
 }
 
 function createGame(game) {
@@ -200,7 +331,32 @@ function createGame(game) {
 				let data = JSON.parse(xhr.responseText);
 				console.log(data);
 				//displayFilm(data);
-				displayGames();
+				loadAllGames();
+			}
+			else {
+				console.error("POST request failed.");
+				console.error(xhr.status + ': ' + xhr.responseText);
+			}
+		}
+	}
+
+	let gameJSON = JSON.stringify(game);
+	console.log(gameJSON);
+	xhr.send(gameJSON);
+}
+
+function updateGame(game) {
+	let xhr = new XMLHttpRequest();
+	xhr.open('PUT', 'api/games/'+game.id, true);
+	xhr.setRequestHeader("Content-Type", "application/json");
+
+	xhr.onreadystatechange = function() {
+		if (xhr.readyState === xhr.DONE) {
+			if (xhr.status === 200) {
+				let data = JSON.parse(xhr.responseText);
+				console.log(data);
+				//displayFilm(data);
+				loadAllGames();
 			}
 			else {
 				console.error("POST request failed.");
@@ -225,6 +381,9 @@ function loadGamesByGenre(genre) {
 				let games = JSON.parse(xhr.responseText)
 				displayGames(games);
 			}
+			else if (xhr.status === 404) {
+				loadAllGames();
+			}
 			else {
 				console.error('Error getting data: ' + xhr.status);
 			}
@@ -243,6 +402,9 @@ function loadGamesByRating(rating) {
 			if (xhr.status === 200) {
 				let games = JSON.parse(xhr.responseText)
 				displayGames(games);
+			}
+			else if (xhr.status === 404) {
+				loadAllGames();
 			}
 			else {
 				console.error('Error getting data: ' + xhr.status);
@@ -263,6 +425,9 @@ function loadGamesByScore(low, high) {
 				let games = JSON.parse(xhr.responseText)
 				displayGames(games);
 			}
+			else if (xhr.status === 404) {
+				loadAllGames();
+			}
 			else {
 				console.error('Error getting data: ' + xhr.status);
 			}
@@ -281,6 +446,8 @@ function loadGamesByKeyword(keyword) {
 			if (xhr.status === 200) {
 				let games = JSON.parse(xhr.responseText)
 				displayGames(games);
+			} else if (xhr.status === 404) {
+				loadAllGames();
 			}
 			else {
 				console.error('Error getting data: ' + xhr.status);
